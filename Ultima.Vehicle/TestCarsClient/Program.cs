d2a725db564758b079e07ac97fde8c046b5ef34c
slonsjo <slonsjo@hotmail.com>
2014-09-25 21:40:00 +0200

    Build..

--- Ultima.Vehicle/TestCarsClient/Program.cs
+++ Ultima.Vehicle/TestCarsClient/Program.cs
@@ -11,14 +11,7 @@ namespace TestCarsClient
     {
         static void Main(string[] args)
         {
-            MqttClient client = new MqttClient(IPAddress.Parse("192.168.10.53"));
-
-            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
-
-            void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
-            {
-            // access data bytes throug e.Message
-            }
+            
         }
     }
 }
