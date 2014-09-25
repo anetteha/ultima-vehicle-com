c2debb0fc59889b0f6a785f61490da9067dd2075
slonsjo <slonsjo@hotmail.com>
2014-09-25 21:53:27 +0200

    TestClient update

--- Ultima.Vehicle/TestCarsClient/Program.cs
+++ Ultima.Vehicle/TestCarsClient/Program.cs
@@ -15,5 +15,41 @@ namespace TestCarsClient
         {
             
         }
+
+        public void Start()
+        {
+            ////http://m2mqtt.wordpress.com/using-mqttclient/
+
+            //MqttClient client = new MqttClient("http://3b5a813b11ad469faa02babc0a6edb45.cloudapp.net/");
+
+            //client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            //void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+            //{
+            //// access data bytes throug e.Message
+            //}
+
+            //client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            //void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+            //{
+            //// access data bytes throug e.Message
+            //}
+            
+            //client.MqttMsgSubscribed += client_MqttMsgSubscribed;
+            //client.MqttMsgUnsubscribed += client_MqttMsgUnsubscribed;
+            //void client_MqttMsgUnsubscribed(object sender, MqttMsgUnsubscribedEventArgs e)
+            //{
+            //// write your code
+            //}
+            //void client_MqttMsgSubscribed(object sender, MqttMsgSubscribedEventArgs e)
+            //{
+            //// write your code
+            //}
+
+            //client.MqttMsgPublished += client_MqttMsgPublished;
+            //void client_MqttMsgPublished(object sender, MqttMsgPublishedEventArgs e)
+            //{
+            //// write your code
+            //}
+        }
     }
 }
